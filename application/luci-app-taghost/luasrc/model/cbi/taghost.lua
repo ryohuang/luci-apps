0f2b9904b87a1e7d355d3a200b76833da1dcb1aa
ryo <anyisdad@icloud.com>
2021-01-16 00:13:12 +0800

    Move device list to top

--- application/luci-app-taghost/luasrc/model/cbi/taghost.lua
+++ application/luci-app-taghost/luasrc/model/cbi/taghost.lua
@@ -1,22 +1,16 @@
 local sys = require "luci.sys"
 local ifaces = sys.net:devices()
+local log = luci.util.perror
+local uci = require("luci.model.uci").cursor()
+
 
 m = Map("dhcp", translate("Tag your device"),
         translatef("Tag a device and set another gateway for it."))
 
-s = m:section(NamedSection, "ext","tag", translate("Set another gateway and dns server for devices."))
--- s.template = "cbi/tblsection"
-s.anonymous = false
-s.addremove = true
 
-tag = s:option(DynamicList, "dhcp_option", translate("Add dhcp_option here.3,means gateway;6,means dns"))
-tag.optional = true
-tag.datatype = "string"
 
-m2 = Map("dhcp", translate("Devices with tags"),
-        translatef("Select your devices"))
-
-devices = m2:section(TypedSection, "host", translate("Bind to tag"))
+-- device list 
+devices = m:section(TypedSection, "host", translate("Bind to tag"))
 devices.template = "cbi/tblsection"
 devices.anonymous = true
 devices.addremove = true
@@ -24,13 +18,6 @@ devices.addremove = true
 a = devices:option(Value, "name", translate("Device Name"))
 a.datatype = "string"
 a.optional = false
-
-function a.validate(self, value)
-        if value == "" then
-                return nil, translate("Device name cannot be empty")
-        end
-        return value
-end
 
 a = devices:option(Value, "mac", translate("MAC Address"))
 a.datatype = "macaddr"
@@ -44,7 +31,21 @@ end)
 a = devices:option(Value, "tag", translate("Device Tag"))
 a.datatype = "string"
 a.optional = false
-a:value('ext')
 
-return m, m2
+uci:foreach("dhcp","tag",
+        function(i)
+                -- log(i['.name'])
+                a:value(i['.name'])
+        end)
+
+-- dhcp section
+t = m:section(TypedSection, "tag", translate("DHCP tags"))
+t.addremove = true
+t.anonymous = false
+
+a = t:option(DynamicList, "dhcp_option", translate("dhcp_option"),translate("3 means gateway and 6 means dns") )
+a.optional = false
+a.datatype = "string"
+
+return  m
 
