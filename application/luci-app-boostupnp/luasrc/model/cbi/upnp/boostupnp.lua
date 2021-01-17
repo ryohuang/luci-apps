6245f576a2960540e5a175a2734f97c81180f628
ryo <anyisdad@icloud.com>
2021-01-17 17:14:35 +0800

    Clean codes

--- application/luci-app-boostupnp/luasrc/model/cbi/upnp/boostupnp.lua
+++ application/luci-app-boostupnp/luasrc/model/cbi/upnp/boostupnp.lua
@@ -5,35 +5,16 @@
 m = Map("upnpd", luci.util.pcdata(translate("Universal Plug & Play")),
 	translate("UPnP allows clients in the local network to automatically configure the router."))
 
--- m:section(SimpleSection).template  = "upnp_status"
 
 s = m:section(NamedSection, "config", "upnpd", translate("MiniUPnP extra settings"))
 s.addremove = false
-s:tab("general",  translate("Settings for DMZ router"))
+s:tab("general",  translate("Settings for router behined DMZ"))
 
 
--- e = s:taboption("general", Flag, "enabled", translate("Start UPnP and NAT-PMP service"))
--- e.rmempty  = false
+s:taboption("general", Value, "external_iface", translate("External Network Interface"),
+	translate("usually is wan")).rmempty = true
 
---function e.cfgvalue(self, section)
---	return luci.sys.init.enabled("miniupnpd") and self.enabled or self.disabled
---end
-
--- function e.write(self, section, value)
--- 	if value == "1" then
--- 		luci.sys.call("/etc/init.d/miniupnpd start >/dev/null")
--- 	else
--- 		luci.sys.call("/etc/init.d/miniupnpd stop >/dev/null")
--- 	end
-
--- 	return Flag.write(self, section, value)
--- end
-
-s:taboption("general", Value, "download", translate("Downlink"),
-	translate("Value in KByte/s, informational only")).rmempty = true
-
-s:taboption("general", Value, "upload", translate("Uplink"),
-	translate("Value in KByte/s, informational only")).rmempty = true
-
+s:taboption("general", Value, "internal_iface", translate("Internal Network Interface"),
+	translate("usually is lan")).rmempty = true
 
 return m
